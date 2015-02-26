c426f3d3a88827868cc1bf90b061a14721bf0fdc
Daniel Salas <d.salas1029@gmail.com>
2015-02-25 17:09:43 -0800

    lots of shit ugh

--- partywithesp/js/main.js
+++ partywithesp/js/main.js
@@ -1,17 +1,18 @@
+/*jshint undef:false */
 
 $( document ).ready(function() {
 
-
+	//// Affix the navbar ////
 	$('#esp-nav').affix({
 		offset: {
-			top: 2
+			top: 10
 		}
 	});
 
-	////  ////
+	//// Rotating text in div ////
 
 	var text = ['Private parties', 'Fundraisers', 'Product launches', 'Holiday parties', 'Bar/Bat Mitvahs', 'Galas', 'Trade shows'];
-        i = 0,
+        var i = 0,
         $div = $('#services');
     
     setInterval(function ()
@@ -23,19 +24,19 @@ $( document ).ready(function() {
     }, 1500);
 
 
-    //// STELLAR & SKROLLR ////
+    //// STELLAR / SKROLLR ////
 
     // var s = skrollr.init();
 
 	if(!Modernizr.touch){ 
-    		$.stellar({
-	  			horizontalScrolling: false,
-	  			responsive: true
-			});
+		$.stellar({
+  			horizontalScrolling: false,
+  			responsive: true
+		});
 
-			$( window ).resize(function() {
-				$.stellar('refresh');
-			});
-		}
+		$( window ).resize(function() {
+			$.stellar('refresh');
+		});
+	}
 
 });
