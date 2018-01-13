421d69fe1de2fd6a38710f7087c68795c0f1b1ba
Daniel Salas <d.salas1029@gmail.com>
2018-01-12 16:36:33 -0800

    Finishing up :)

--- versing/js/scripts.js
+++ versing/js/scripts.js
@@ -1,4 +1,12 @@
+ // REMOVE THIS BEFORE GOING LIVE
+ /*jslint devel: true */
+
  $(document).ready(function(){
+ 	// repeat versing on click
+ 	$('.versing-title').on('click', function(){
+ 		var versing = $(this);
+ 		versing.append("<br>VERSING");
+ 	});
 
  	// Show or hide album embeds based on click
  	var albumEmbed = {
@@ -8,25 +16,81 @@
  	};
 
  	var previous = '';
- 	var change = true;
+ 	var previousAlbum = '';
 
  	$('#album-covers img').on('click', function(){
  		var currentImage = $(this);
  		var current = this.id;
- 		console.log("current is " + current);
 
- 		if( (previous === current) && change ) {
- 			console.log("previous is also current");
- 			$(albumEmbed[current]).removeClass('show');
- 			currentImage.removeClass('active');
- 			change = false;
- 		} else {
- 			console.log("showing " + current);
- 			$(albumEmbed[current]).addClass('show');
- 			$(albumEmbed[previous]).removeClass('show');
- 			currentImage.addClass('active');
- 		}
- 		previous = current;
+		if (current === previous) { // if the current one we're clicking is the same as the last one we clicked
+			$(albumEmbed[current]).toggleClass('show');
+			currentImage.toggleClass('active');
+		}
+		else {
+			$(albumEmbed[current]).addClass('show');
+			$(albumEmbed[previous]).removeClass('show');
+			currentImage.addClass('active');
+			if (previousAlbum) {
+				previousAlbum.removeClass('active');
+			}
+		}
+		previous = current;
+		previousAlbum = currentImage;
  	});
+
+ 	if ($(window).width() > 768){
+	 	$('#cmoThumb').on('mouseover', function() {
+	 		$(this).attr('src', '../img/videos/cmo.webp');
+	 	});
+		$('#cmoThumb').on('mouseleave', function() {
+	 		$(this).attr('src', '../img/videos/cmo.jpg');
+	 	});
+
+	 	$('#vkThumb').on('mouseover', function() {
+	 		$(this).attr('src', '../img/videos/vk.webp');
+	 	});
+		$('#vkThumb').on('mouseleave', function() {
+	 		$(this).attr('src', '../img/videos/vk.jpg');
+	 	});
+
+	 	$('#kexpThumb').on('mouseover', function() {
+	 		$(this).attr('src', '../img/videos/kexp.webp');
+	 	});
+		$('#kexpThumb').on('mouseleave', function() {
+	 		$(this).attr('src', '../img/videos/kexp.jpg');
+	 	});
+	}
+
+	// Show or hide video embeds based on click
+ 	var videoEmbed = {
+ 		cmoThumb: '#cmo-embed',
+ 		vkThumb: '#vk-embed',
+ 		kexpThumb: '#kexp-embed'
+ 	};
+
+ 	var previous2 = '';
+ 	var previousThumb = '';
+
+ 	$('#video-thumbs img').on('click', function(){
+ 		var currentThumb = $(this);
+ 		var current2 = this.id;
+
+		if (current2 === previous2) { // if the current one we're clicking is the same as the last one we clicked
+			$(videoEmbed[current2]).toggleClass('show');
+			currentThumb.toggleClass('active');
+		}
+		else {
+			$(videoEmbed[current2]).addClass('show');
+			$(videoEmbed[previous2]).removeClass('show');
+			currentThumb.addClass('active');
+			if (previousThumb) {
+				previousThumb.removeClass('active');
+			}
+		}
+		previous2 = current2;
+		previousThumb = currentThumb;
+ 	});
+
+ 	$('.youtube-embed').fitVids();
 
 });
